67a55118e93cc6d8255aa350214fc0f6b7e1af50
Jindřich Káňa <jindrich.kana@elostech.cz>
2018-04-03 11:24:08 +0200

    init

---
+++ addreview.php
@@ -0,0 +1,29 @@
+<?php
+
+$hostname = "db:3306";
+$username = "hodnoceni";
+$password = "hodnoceni";
+$db = "reviews";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['submit'])) {
+  $reviewer_name=$_POST['reviewer_name'];
+  $star_rating=$_POST['star_rating'];
+  $details=$_POST['details'];
+
+  $query = "INSERT INTO user_review (reviewer_name, star_rating, details)
+  VALUES ('$reviewer_name', '$star_rating', '$details')";
+
+    if (!mysqli_query($dbconnect, $query)) {
+        die('Chyba, posouzení se neuložilo..');
+    } else {
+      echo "Díky ;)";
+    }
+
+}
+?>
